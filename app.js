87f4fe4a3d70ae1633b55a34cf64937919639ec3
mendhak <smaltaf@msn.com>
2014-07-03 23:11:50 +0100

    First method, get user nsid

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -14,15 +14,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
+//app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//All routing goes to index.js
 app.use('/', routes);
-app.use('/users', users);
+
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
+var apiKey = "a39dfdf51784c76fa3234f88bec38b0e";
 var express = require('express');
 var router = express.Router();
+var flickr;
+var Flickr = require("flickrapi"),
+    flickrOptions = {
+        api_key: apiKey //,
+        //secret: "API key secret that you get from Flickr"
+    };
+Flickr.tokenOnly(flickrOptions, function(error, flickrapi) {
+    flickr = flickrapi;
+
+});
+
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/nsid/:username', function(req, res){
+    var nsid = flickr.urls.lookupUser( {url :"http://www.flickr.com/photos/mendhak" } , function(err, result){
+        res.send(result.user.id);
+    });
+
+
+});
+
+
+
+
+
+
 
 module.exports = router;
