69f833d931fe7144c90b71dc14338a00e29766c3
mendhak <smaltaf@msn.com>
2014-07-04 23:14:39 +0100

    NSID and Img API endpoints

--- routes/index.js
+++ routes/index.js
@@ -5,31 +5,102 @@ var flickr;
 var Flickr = require("flickrapi"),
     flickrOptions = {
         api_key: apiKey //,
-        //secret: "API key secret that you get from Flickr"
     };
 Flickr.tokenOnly(flickrOptions, function(error, flickrapi) {
     flickr = flickrapi;
-
 });
 
 
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
 
-router.get('/nsid/:username', function(req, res){
-    var nsid = flickr.urls.lookupUser( {url :"http://www.flickr.com/photos/mendhak" } , function(err, result){
-        res.send(result.user.id);
+
+router.get('/img/:nsid/:num?/:size?/:popular?', function(req, res){
+
+    var num = 1;
+    var size = 's';
+    var popular = 'date-posted-desc';
+    var username = req.params.nsid;
+
+    if(req.params.num && !isNaN(req.params.num)){
+        num = req.params.num;
+    }
+
+    if(req.params.size){
+        size = req.params.size;
+    }
+
+    if(req.params.popular && req.params.popular=='p'){
+        popular = 'interestingness-desc';
+    }
+
+    getUserNsid(username, req.cookies, function(nsid, err){
+        if(nsid){
+            res.cookie("nsid_"+username, nsid, { maxAge: 3600000, path: '/' });
+
+            flickr.photos.search({
+                user_id: nsid,
+                per_page: 1,
+                page: num,
+                sort: popular,
+                extras: "url_"+size
+            }, function(err, result){
+                if(!err && result.photos && result.photos.photo.length > 0){
+                    res.redirect(result.photos.photo[0]["url_"+size]);
+                } else {
+                    res.send("", 400);
+                }
+            });
+        }
+        else{
+            res.setHeader("X-Error", err);
+            res.send("", 400);
+        }
     });
-
-
 });
 
+function getUserNsid(username, cookies, callback) {
 
+    //If it contains @, it's already an NSID
+    if(username.indexOf('@') > -1){
+        return username;
+    }
 
+    //Check if a cookie already contains this value
+    if(cookies["nsid_" + username] && cookies["nsid_" + username].length > 0){
+        callback(cookies["nsid_" + username], null);
+        return;
+    }
+
+    //Call the Flickr API
+    flickr.urls.lookupUser( {url :"http://www.flickr.com/photos/" + username } , function(err, result){
+        if(!err && result.user){
+            callback(result.user.id, null);
+        } else {
+            console.log(err);
+            callback(null, err);
+        }
+    });
+
+}
+
+router.get('/nsid/:username', function(req, res){
+
+    var username = req.params.username;
+    getUserNsid(username, req.cookies, function(nsid, err){
+        if(nsid){
+            res.cookie("nsid_"+username, nsid, { maxAge: 3600000, path: '/' });
+            res.send(nsid);
+        }
+        else{
+            res.setHeader("X-Error", err);
+            res.send("", 400);
+        }
+    });
+});
 
 
 
