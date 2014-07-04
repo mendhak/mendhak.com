f34c17e481d88c920afb7ad989657e667fbb821a
mendhak <smaltaf@msn.com>
2014-07-05 00:54:17 +0100

    Front page for signature generator

--- routes/index.js
+++ routes/index.js
@@ -13,15 +13,53 @@ Flickr.tokenOnly(flickrOptions, function(error, flickrapi) {
 
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', domain: req.headers.host });
 });
 
 
+function getImageUrl(photo, size) {
 
+    var sizePrefix = "_";
+
+    if(size == 'x'){
+        sizePrefix = '';
+        size = '';
+    }
+
+    size = sizePrefix + size;
+
+    return "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + size + ".jpg";
+
+//    if size == 'small' or size == '240':
+//    size = 'm'
+//    if size == 'small320' or size == '320':
+//    size = 'n'
+//    elif size == 'square' or size == '75':
+//    size = 's'
+//    elif size == 'largesquare' or size == '150':
+//    size = 'q'
+//    elif size == 'thumb' or size == 'thumbnail' or size == 'tiny' or size == '100':
+//    size = 't'
+//    elif size == 'medium640' or size == '640':
+//    size = 'z'
+//    elif size == 'medium800' or size == '800':
+//    size = 'c'
+//    elif size == 'large' or size == 'big' or size == '1024':
+//    size = 'b'
+//    elif size == '' or size == 'medium' or size == 'med' or size == 'x' or size == '500':
+//    sizePrefix =  ''
+//    size = ''
+//
+//    size = sizePrefix + size
+//
+//    return "http://farm{0}.static.flickr.com/{1}/{2}_{3}{4}.jpg".format(selectedPhoto.farm, selectedPhoto.server, selectedPhoto.id, selectedPhoto.secret, size)
+
+}
 router.get('/img/:nsid/:num?/:size?/:popular?', function(req, res){
 
     var num = 1;
-    var size = 's';
+
+    var size = 'm';
     var popular = 'date-posted-desc';
     var username = req.params.nsid;
 
@@ -45,11 +83,11 @@ router.get('/img/:nsid/:num?/:size?/:popular?', function(req, res){
                 user_id: nsid,
                 per_page: 1,
                 page: num,
-                sort: popular,
-                extras: "url_"+size
+                sort: popular
             }, function(err, result){
                 if(!err && result.photos && result.photos.photo.length > 0){
-                    res.redirect(result.photos.photo[0]["url_"+size]);
+                    var imgUrl = getImageUrl(result.photos.photo[0], size);
+                    res.redirect(imgUrl);
                 } else {
                     res.send("", 400);
                 }
@@ -66,7 +104,8 @@ function getUserNsid(username, cookies, callback) {
 
     //If it contains @, it's already an NSID
     if(username.indexOf('@') > -1){
-        return username;
+        callback(username, null);
+        return;
     }
 
     //Check if a cookie already contains this value
@@ -87,9 +126,18 @@ function getUserNsid(username, cookies, callback) {
 
 }
 
-router.get('/nsid/:username', function(req, res){
+router.get('/nsid/*', function(req, res){
 
-    var username = req.params.username;
+    var username = req.params[0];
+
+    if(username.indexOf('http://') > -1){
+        var regexHttp = /photos\/([^/]+)\/?/;
+        var match = regexHttp.exec(username);
+        username = match[1];
+//        var match = myRegexp.exec(myString);
+//        alert(match[1]);  // abc
+    }
+
     getUserNsid(username, req.cookies, function(nsid, err){
         if(nsid){
             res.cookie("nsid_"+username, nsid, { maxAge: 3600000, path: '/' });
